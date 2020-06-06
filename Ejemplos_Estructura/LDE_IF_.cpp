91deff247c58e0f2add3f63f6a4c00ccdf6dd59c
LaMeraChuleta <geoffreyytorres25@gmail.com>
2020-06-06 18:54:56 -0500

    ListaDoblementeEnlazaI_FTerminada

--- Ejemplos_Estructura/LDE_IF_.cpp
+++ Ejemplos_Estructura/LDE_IF_.cpp
@@ -1,101 +1,297 @@
-
 #include <iostream>
 using namespace std;
 
-class Nodo
-{
-private:
-    int dato;
-    Nodo *sig;
-    Nodo *ant;
-public:
-    Nodo()
-    {    dato=0;
-        sig=NULL;
-        ant=NULL;
-    }
-    Nodo(int x)
-    {    dato=x;
-        sig=NULL;
-        ant=NULL;
-    }
-    void Asignasig(Nodo*);
-    void Asignaant(Nodo*);
-    void Leer(int);
-    void Imprimir();
-    Nodo *Obtienesig();
-    Nodo *Obtieneant();
-    int Obtienedato();
-};
-class LDE
-{    private:
-    Nodo *Inicio, *Fin;
-public:
-    void InsertarI(int);
-    void InsertarF(int);
-    void BorrarI();
-    void BorrarF();
-    void Imprimir();
-    void ImprimirR();
-    int Contar();
-    Nodo *Buscar(int);
-    void Borrar(int);
-    LDE()
-    {
-        Inicio=NULL;
-        Fin=NULL;
-        
-    }
-    
+
+//Inicio Clase Nodo
+class Nodo{
+	
+	private:
+	
+    	int dato;
+    	Nodo *sig;
+    	Nodo *ant;
+	public:
+	
+	    Nodo(){    
+	    
+			dato=0;
+        	sig=NULL;
+        	ant=NULL;
+    	}
+    	Nodo(int x){    
+    	
+			dato=x;
+        	sig=NULL;
+        	ant=NULL;
+    	}
+    	void Asignasig(Nodo*);
+    	void Asignaant(Nodo*);
+    	void Leer(int);
+    	void Imprimir();
+    	Nodo *Obtienesig();
+    	Nodo *Obtieneant();
+    	int Obtienedato();
 };
 
+//Inicio Clase LDE_I_F
+class LDE{    
 
+	private:
+		
+    	Nodo *Inicio, *Fin;
+	public:
+		
+    	void InsertarI(int);
+    	void InsertarF(int);
+    	void BorrarI();
+    	void BorrarF();
+    	void Imprimir();
+    	void ImprimirR();
+    	int Contar();
+    	Nodo *Buscar(int);
+    	void Borrar(int);
+    	LDE(){
+    		
+        	Inicio=NULL;
+        	Fin=NULL;        
+    	}    
+};
 
-
-
-void Nodo::Asignasig(Nodo *x)
-{
+//Metodo Clase Nodo
+void Nodo::Asignasig(Nodo *x){
+	
     this->sig=x;
 }
-void Nodo::Asignaant(Nodo *x)
-{
+void Nodo::Asignaant(Nodo *x){
+	
     this->ant=x;
 }
-
-
-void Nodo::Leer(int x)
-{
+void Nodo::Leer(int x){
+	
     this->dato=x;
 }
-void Nodo::Imprimir()
-{
+void Nodo::Imprimir(){
+	
     cout<<this->dato<<endl;
 }
-Nodo* Nodo::Obtienesig()
-{
+Nodo* Nodo::Obtienesig(){
+	
     return(this->sig);
 }
-
-Nodo* Nodo::Obtieneant()
-{
+Nodo* Nodo::Obtieneant(){
+	
     return(this->ant);
 }
-
-
-int Nodo::Obtienedato()
-{
+int Nodo::Obtienedato(){
+	
     return(this->dato);
 }
 
+//Metodos Clase LDE_I_F
+
+Nodo* LDE::Buscar(int x){
+	
+	if(Inicio == NULL){
+		
+		cout<<"Lista vacia :("<<endl;
+		return NULL;		
+	}
+	else{
+		
+		Nodo *help = Inicio;				
+	
+		while(help != NULL){
+										
+			if(help->Obtienedato() == x){
+				return help;
+			}
+			help = help->Obtienesig();					
+		}
+		return NULL;
+	}			
+}
+void LDE::BorrarI(){
+	
+	if(Inicio == NULL){
+		
+		cout<<"Lista vacia :("<<endl;
+	}
+	else{
+		
+		if(Inicio->Obtienesig() == NULL){
+			
+			Inicio = NULL;
+		}
+		else{
+			
+			Nodo *aux = Inicio->Obtienesig();									
+			aux->Asignaant(NULL);
+			Inicio->Asignasig(NULL);
+			delete Inicio;			
+			Inicio = aux;									
+		}				
+	}
+}
+void LDE::BorrarF(){
+	
+	if(Inicio == NULL){
+		
+		cout<<"Lista vacia :("<<endl;
+	}
+	else{
+		
+		if(Inicio->Obtienesig() == NULL){
+			
+			Inicio = NULL;
+			Fin = NULL;
+		}
+		else{		
+			
+			Nodo *aux = Fin;
+			
+			Fin = aux->Obtieneant();
+			aux->Asignaant(NULL);
+			Fin->Asignasig(NULL);			
+			delete aux;
+				
+		}				
+	}
+}
+void LDE::Borrar(int x){
+
+	if(Inicio == NULL){		
+	
+		cout<<"Lista vacia :("<<endl;		
+	}
+	else{
+												
+		if(this->Buscar(x) != NULL){
+	
+			Nodo *aux = Inicio;	
+			while(aux->Obtienesig() != NULL){
+						
+				if(aux->Obtienedato() == x){				
+					break;
+				}
+				aux = aux->Obtienesig();
+			}			
+			
+			if(aux->Obtieneant() == NULL){
+				
+				this->BorrarI();
+			}
+			else if(aux->Obtienesig() == NULL){
+				
+				this->BorrarF();
+			}
+			else{
+						
+				Nodo *help = aux->Obtieneant();
+				help->Asignasig(aux->Obtienesig());
+			
+				aux->Asignasig(NULL);
+			
+				help = help->Obtienesig();
+				help->Asignaant(aux->Obtieneant());			
+				aux->Asignaant(NULL);
+				delete aux;	
+			}	
+		}
+		else{
+			cout<<"No existe ese numero en la lista!!"<<endl;
+		}
+	}	
+}
+void LDE::InsertarI(int x){
+	
+	if(Inicio == NULL){
+		
+		Inicio = new Nodo(x);
+		Fin	= Inicio;				
+	}	
+	else{	
+					
+		Nodo *newNodo = new Nodo(x);				
+		Nodo *aux = Inicio;
+		Inicio = newNodo;
+		Inicio->Asignasig(aux);
+		aux->Asignaant(Inicio);																		
+	}	
+}
+void LDE::InsertarF(int x){
+	
+	if(Inicio == NULL){
+		
+		Inicio = new Nodo(x);
+		Fin = Inicio;
+	}	
+	else{	
+						
+		Nodo *newNodo = new Nodo(x);		
+		Fin->Asignasig(newNodo);		
+		newNodo->Asignaant(Fin);		
+		Fin = newNodo;								
+	}
+}
+void LDE::Imprimir(){
+	
+	if(Inicio == NULL){
+				
+		cout<<"Lista Vacio :("<<endl;
+	}
+	else{				 
+	Nodo *help = Inicio;				
+	
+	while(help != NULL){
+										
+			help->Imprimir();
+			help = help->Obtienesig();					
+		}
+	}
+}
+void LDE::ImprimirR(){
+	
+	if(Inicio == NULL){
+				
+		cout<<"Lista Vacio :("<<endl;
+	}
+	else{				 
+		Nodo *help = Fin;				
+	
+		while(help != NULL){
+											
+			help->Imprimir();		
+			help = help->Obtieneant();								
+		}					
+	}	
+}
+int LDE::Contar(){
+	
+	if(Inicio == NULL){
+				
+		cout<<"Lista Vacio :("<<endl;
+		return 0;
+	}
+	else{				 
+		Nodo *help = Inicio;
+		int contador = 1;				
+	
+		while(help->Obtienesig() != NULL){
+									
+			contador++;																			
+			help = help->Obtienesig();								
+		}
+		return contador;				
+	}		
+}
 
 
 
 
-int main(int argc, const char * argv[])
-{
-    
-    
-    
+
+
+int main(int argc, const char * argv[]){            
+
     LDE A;
     int opc, dato;
     do{
@@ -111,39 +307,42 @@ int main(int argc, const char * argv[])
         cout<<"Teclee la opcion"<<endl;
         cin>>opc;
         
+        system("cls");
+        
         switch(opc)
         {
             case 1:
                 cout<<"teclee el dato a insertar"<<endl;
                 cin>>dato;
                 A.InsertarI(dato);
-                break;
+                break;        	
             case 2:
                 cout<<"Teclee el dato a insertar"<<endl;
                 cin>>dato;
                 A.InsertarF(dato);
-                break;
+                break;            
             case 3:
                 A.BorrarI();
-                break;
+                break;        
             case 4:
                 A.BorrarF();
-                break;
+                break;        	
             case 5:
                 dato=A.Contar();
                 cout<<"Hay "<<dato<<" nodos en la Lista"<<endl;
-                break;
+                break;            
             case 6:
                 cout<<"Teclee el dato a borrar"<<endl;
                 cin>>dato;
                 A.Borrar(dato);
-                break;
+                break;                
             case 7:
                 A.Imprimir();
                 break;
             case 8:
                 A.ImprimirR();
                 break;
+            
             default:
                 cout<<"opción salir...";
                 break;
